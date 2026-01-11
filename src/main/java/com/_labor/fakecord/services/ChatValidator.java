f5a478cbfc34ad87ef0368524c43d4ab424d9bf7
2labor <yaroslavkliutko@gmail.com>
2026-01-11 18:13:43 +0000

    Basic message validation

---
+++ src/main/java/com/_labor/fakecord/services/ChatValidator.java
@@ -0,0 +1,26 @@
+package com._labor.fakecord.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com._labor.fakecord.model.ChatMessage;
+
+@Service
+public class ChatValidator {
+  public Optional<ChatMessage> validate(ChatMessage message) {
+    return Optional.ofNullable(message)
+    .filter(this::isUserNameValid)
+    .filter(this::isContentValid);
+  }
+
+  private boolean isUserNameValid(ChatMessage m) {
+    String name = m.getUserName();
+    return name != null && !name.trim().isEmpty() && name.length() >= 2 && name.length() <= 20;
+  }
+
+  private boolean isContentValid(ChatMessage m) {
+    String content = m.getContent();
+    return content != null && !content.trim().isEmpty() && content.length() <= 500;
+  }
+}
