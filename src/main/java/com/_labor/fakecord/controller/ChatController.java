07cc92f1a7db4fb334fff00ba34843a51361d326
2labor <yaroslavkliutko@gmail.com>
2026-01-11 18:13:04 +0000

    Basic message validation

--- src/main/java/com/_labor/fakecord/controller/ChatController.java
+++ src/main/java/com/_labor/fakecord/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com._labor.fakecord.controller;
 
+import java.util.Optional;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -7,14 +9,26 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import com._labor.fakecord.model.ChatMessage;
+import com._labor.fakecord.model.MessageType;
+import com._labor.fakecord.services.ChatValidator;
 
 @Controller
 public class ChatController {
 
+  private final ChatValidator chatValidator;
+
+  public ChatController(ChatValidator chatValidator) {
+    this.chatValidator = chatValidator;
+  }
+
   @MessageMapping("/chat.sendMessage")
   @SendTo("/topic/public")
   public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-    return chatMessage;
+    return chatValidator.validate(chatMessage)
+      .orElseGet(() ->{
+        System.out.println("LOG: Invalid message dropped");
+        return null;
+      });
   }
 
   @MessageMapping("/chat.addUser")
@@ -23,7 +37,13 @@ public class ChatController {
     @Payload ChatMessage chatMessage,
     SimpMessageHeaderAccessor headerAccessor 
   ) {
-    headerAccessor.getSessionAttributes().put("username", chatMessage.getUserName());
-    return chatMessage;
+    return chatValidator.validate(chatMessage).map(validMsg -> {
+      headerAccessor.getSessionAttributes().put("username", validMsg.getUserName());
+      return validMsg;
+    }).orElseGet(() -> {
+      chatMessage.setType(MessageType.ERROR);
+      chatMessage.setContent("User name has to be from 2 characters to 20!");
+      return chatMessage;
+    });
   }
 }
--- src/main/java/com/_labor/fakecord/model/MessageType.java
+++ src/main/java/com/_labor/fakecord/model/MessageType.java
@@ -3,5 +3,6 @@ package com._labor.fakecord.model;
 public enum MessageType {
   CHAT,
   JOIN,
-  LEAVE 
+  LEAVE,
+  ERROR
 }
