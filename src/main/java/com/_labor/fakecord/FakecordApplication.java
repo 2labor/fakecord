360534932d3501e162e5a0d3722161ca0e879748
2labor <yaroslavkliutko@gmail.com>
2026-01-11 15:13:35 +0000

    Initial commit

---
+++ src/main/java/com/_labor/fakecord/FakecordApplication.java
@@ -0,0 +1,13 @@
+package com._labor.fakecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakecordApplication.class, args);
+	}
+
+}
